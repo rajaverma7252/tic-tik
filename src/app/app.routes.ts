0f47982a2909c903bbc6bbc6f8acbcb0e411e8dc
rajaverma <rajaverma@policybazaar.com>
2024-03-23 15:55:37 +0530

    added login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home/home.component';
+import { AuthService } from './services/auth.service';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
+    { path: 'home', component: HomeComponent, canActivate: [AuthService] },
     { path: '', redirectTo: 'login', pathMatch: 'full' },
     { path: '**', redirectTo: 'login', pathMatch: 'full' }
 ];
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,49 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
+
 export class LoginComponent {
 
+  userId: string = "";
+  password: string = "";
+
+  constructor(
+    private router: Router
+  ) { }
+
+  login(userId: string, password: string) {
+    if (userId == 'raja' && password == 'raja@123') {
+      sessionStorage.setItem('login', "true");
+      Swal.fire({
+        // title: "Good job!",
+        text: "Login Successfully",
+        icon: "success",
+        backdrop: false,
+        showConfirmButton: false,
+        footer: 'Redirecting to home page...',
+        timer: 1000,
+      });
+      setTimeout(() => {
+        this.router.navigate(['/home']);
+      }, 1000);
+    } else {
+      Swal.fire({
+        // title: 'Error!',
+        text: 'Invalid UserId/Password',
+        icon: 'error',
+        confirmButtonText: 'Try again',
+        backdrop: true
+      })
+    }
+  }
 }
